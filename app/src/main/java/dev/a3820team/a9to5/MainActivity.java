8e6f2985a2d17b5567fe2d284411494149c57e60
Ani Mehta <tenderanger@gmail.com>
2018-03-23 12:25:12 +1000

    initial commit

---
+++ app/src/main/java/dev/a3820team/a9to5/MainActivity.java
@@ -0,0 +1,13 @@
+package dev.a3820team.a9to5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
