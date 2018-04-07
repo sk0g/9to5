905f2c48a23e53abc9bf90a200480d5f79d82ab8
Ani Mehta <tenderanger@gmail.com>
2018-04-08 02:27:05 +1000

    Corrected file names, start work on scrollable question list

--- app/src/main/java/dev/a3820team/a9to5/MeQuestionsActivity.java
+++ app/src/main/java/dev/a3820team/a9to5/MeQuestionsActivity.java
@@ -1,9 +1,13 @@
 package dev.a3820team.a9to5;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by sk0g on 07/04/2018.
+ */
+
+public class MeQuestionsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/dev/a3820team/a9to5/QuizQuestions.java
+++
@@ -1,9 +0,0 @@
-package dev.a3820team.a9to5;
-
-/**
- * Created by sk0g on 07/04/2018.
- */
-
-public class QuizQuestions {
-    static String[] mQuestionsArray
-}
