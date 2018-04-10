595ed88d3d8b38e08662d89e872d612eccb4ef02
Ani Mehta <tenderanger@gmail.com>
2018-04-10 20:07:45 +1000

    Finalised scrolling, implemented starting the quiz

--- app/src/main/java/dev/a3820team/a9to5/WelcomeActivity.java
+++ app/src/main/java/dev/a3820team/a9to5/WelcomeActivity.java
@@ -3,6 +3,8 @@ package dev.a3820team.a9to5;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -11,6 +13,7 @@ import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    Button mStartQuizButton;
     TextView mMainText;
 
     @Override
@@ -20,5 +23,17 @@ public class WelcomeActivity extends AppCompatActivity {
 
         mMainText = (TextView) findViewById(R.id.main_screen_long_text);
         mMainText.setText(getText(R.string.welcome_page_middle_text));
+
+        mStartQuizButton = (Button) findViewById(R.id.start_quiz_button);
+        mStartQuizButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchToQuiz();
+            }
+        });
+    }
+
+    void switchToQuiz() {
+        setContentView(R.layout.activity_me_questions);
     }
 }
