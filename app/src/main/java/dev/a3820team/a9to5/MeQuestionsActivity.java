81a5b94c34502842a4c67e0fb1a468beb5c229f6
Ani Mehta <tenderanger@gmail.com>
2018-04-12 15:18:33 +1000

    App readied for demo

--- app/src/main/java/dev/a3820team/a9to5/MeQuestionsActivity.java
+++ app/src/main/java/dev/a3820team/a9to5/MeQuestionsActivity.java
@@ -1,19 +1,38 @@
 package dev.a3820team.a9to5;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Arrays;
+
 public class MeQuestionsActivity extends AppCompatActivity {
 
     Button mSubmitButton;
-    RadioGroup[] mAnswerRadioGroups = new RadioGroup[13];
+    final int[] mRadioGroupID = {
+            R.id.me_radio_group_1,
+            R.id.me_radio_group_2,
+            R.id.me_radio_group_3,
+            R.id.me_radio_group_4,
+            R.id.me_radio_group_5,
+            R.id.me_radio_group_6,
+            R.id.me_radio_group_7,
+            R.id.me_radio_group_8,
+            R.id.me_radio_group_9,
+            R.id.me_radio_group_10,
+            R.id.me_radio_group_11,
+            R.id.me_radio_group_12,
+            R.id.me_radio_group_13
+    };
+    int[] mSelectedOptions = new int[13];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,21 +46,39 @@ public class MeQuestionsActivity extends AppCompatActivity {
                 toastScore();
             }
         });
-
-        int num = 1;
-        String radioGroupName;
-        int radioGroupNum;
-        for (int i = 0; i < 13; i++) {
-            radioGroupName = "R.id.me_radio_group_" + num;
-            radioGroupNum = getResources().getIdentifier(radioGroupName, "id", getPackageName());
-
-            mAnswerRadioGroups[i] = (RadioGroup) findViewById(radioGroupNum);
-
-            num += 1;
-        }
     }
 
     void toastScore() {
-        Toast.makeText(MeQuestionsActivity.this, "this will show the score one day", Toast.LENGTH_LONG).show();
+        updateScores();
+
+        boolean containsZero = false;
+        for (int i : mSelectedOptions) {
+            if (i == -1) { containsZero = true; break; }
+        }
+
+        if (containsZero) {
+            Toast.makeText(MeQuestionsActivity.this, "Some questions remain unanswered", Toast.LENGTH_LONG).show();
+        } else {
+            int sum = 0;
+            for (int i : mSelectedOptions) { sum += i; }
+            Toast.makeText(MeQuestionsActivity.this, Integer.toString(sum), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    void updateScores() {
+        for (int i = 0; i < 13; i++) {
+            RadioGroup radioGroup = (RadioGroup) findViewById(mRadioGroupID[i]);
+
+            int buttonID = radioGroup.getCheckedRadioButtonId();
+
+            if (buttonID == -1) { mSelectedOptions[i] = -1; break; }
+            else {
+                String checkedId = getResources().getResourceEntryName(buttonID);
+
+                if (checkedId.endsWith("1")) { mSelectedOptions[i] = 1; }
+                else if (checkedId.endsWith("2")) { mSelectedOptions[i] = 2; }
+                else if (checkedId.endsWith("3")) { mSelectedOptions[i] = 3; }
+            }
+        }
     }
 }
