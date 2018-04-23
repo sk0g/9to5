978627e4c989f8fcb5f7c2969f9c351e756c7f70
Ani Mehta <tenderanger@gmail.com>
2018-04-23 15:19:13 +1000

    Added buttons to MeResultsActivity
    only thing needing to be done for demo-readiness is loading of previous answers in MeQuestionsActivity

--- app/src/main/java/dev/a3820team/a9to5/MeResultsActivity.java
+++ app/src/main/java/dev/a3820team/a9to5/MeResultsActivity.java
@@ -1,10 +1,12 @@
 package dev.a3820team.a9to5;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,7 +15,8 @@ import static dev.a3820team.a9to5.MeQuestionsActivity.ME_PREFS_NAME;
 public class MeResultsActivity extends AppCompatActivity {
 
     int mTotalScore;
-//    TextView mResultBox, mFortuneType, mShortResults, mLongResults;
+    TextView mResultBox, mFortuneType, mResultText, mResultQuestion;
+    Button mPreviousButton, mNextButton;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
@@ -26,26 +29,59 @@ public class MeResultsActivity extends AppCompatActivity {
 
     private void displayResults() {
 
-        TextView mResultBox    = (TextView) findViewById(R.id.me_results_box);
-        TextView mFortuneType  = (TextView) findViewById(R.id.me_fortune_type);
-        TextView mShortResults = (TextView) findViewById(R.id.me_result_short);
-        TextView mLongResults  = (TextView) findViewById(R.id.me_results_long);
+        mPreviousButton = (Button) findViewById(R.id.activity_me_results_previous_button);
+        mPreviousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchToMeQuiz();
+            }
+        });
+        mNextButton     = (Button) findViewById(R.id.activity_me_results_next_button);
+        // TODO: Listener to switch to next quiz activity
+
+        mResultBox      = (TextView) findViewById(R.id.me_results_box);
+        mFortuneType    = (TextView) findViewById(R.id.me_fortune_type);
+        mResultText     = (TextView) findViewById(R.id.me_results);
+        mResultQuestion = (TextView) findViewById(R.id.me_results_question);
 
         mResultBox.setText(Integer.toString(mTotalScore));
 
-        int colour;
+        int colour, fortuneBoxTitleID, resultTextID;
         if (mTotalScore <= 35) {
             colour = getResources().getColor(R.color.customRed);
+            fortuneBoxTitleID = R.string.misfortune_title;
         } else if (mTotalScore <= 50) {
             colour = getResources().getColor(R.color.customYellow);
+            fortuneBoxTitleID = R.string.midfortune_title;
         } else {
             colour = getResources().getColor(R.color.customGreen);
+            fortuneBoxTitleID = R.string.fortune_title;
         }
 
+        if (mTotalScore <= 30) {
+            resultTextID = R.string.me_fortune_very_low_text;
+        } else if (mTotalScore <= 40) {
+            resultTextID = R.string.me_fortune_low_text;
+        } else if (mTotalScore <= 50) {
+            resultTextID = R.string.me_fortune_medium_text;
+        } else {
+            resultTextID = R.string.me_fortune_high_text;
+        }
+
+        mFortuneType.setText(getResources().getString(fortuneBoxTitleID));
+        mResultText.setText(getResources().getString(resultTextID));
+        mResultQuestion.setText(getResources().getString(R.string.post_me_text));
+
+        mFortuneType.setTextColor(colour);
         mResultBox.setTextColor(colour);
 
         ImageView drawRectangle = findViewById(R.id.me_results_drawable);
         drawRectangle.setColorFilter(colour);
+    }
+
+    private void switchToMeQuiz() {
+        Intent quizIntent = new Intent(this, MeQuestionsActivity.class);
+        startActivity(quizIntent);
     }
 
     private void getTotalMeScore() {
