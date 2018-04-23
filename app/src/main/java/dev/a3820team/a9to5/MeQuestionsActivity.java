61fe83e8108b48977b261394c5bde34a63dee3fd
Ani Mehta <tenderanger@gmail.com>
2018-04-23 15:49:11 +1000

    Demo ready
    I think...

--- app/src/main/java/dev/a3820team/a9to5/MeQuestionsActivity.java
+++ app/src/main/java/dev/a3820team/a9to5/MeQuestionsActivity.java
@@ -15,51 +15,21 @@ public class MeQuestionsActivity extends AppCompatActivity {
 
     public static final String ME_PREFS_NAME = "dev.a3820team.a9to5.me_questions";
 
-    Button mSubmitButton;
+    Button mSubmitButton, mPreviousButton;
     final int[] mRadioGroupID = {
-            R.id.me_radio_group_1,
-            R.id.me_radio_group_2,
-            R.id.me_radio_group_3,
-            R.id.me_radio_group_4,
-            R.id.me_radio_group_5,
-            R.id.me_radio_group_6,
-            R.id.me_radio_group_7,
-            R.id.me_radio_group_8,
-            R.id.me_radio_group_9,
-            R.id.me_radio_group_10,
-            R.id.me_radio_group_11,
-            R.id.me_radio_group_12,
-            R.id.me_radio_group_13,
-            R.id.me_radio_group_14,
-            R.id.me_radio_group_15,
-            R.id.me_radio_group_16,
-            R.id.me_radio_group_17,
-            R.id.me_radio_group_18,
-            R.id.me_radio_group_19,
-            R.id.me_radio_group_20
+            R.id.me_radio_group_1,  R.id.me_radio_group_2,  R.id.me_radio_group_3,  R.id.me_radio_group_4,
+            R.id.me_radio_group_5,  R.id.me_radio_group_6,  R.id.me_radio_group_7,  R.id.me_radio_group_8,
+            R.id.me_radio_group_9,  R.id.me_radio_group_10, R.id.me_radio_group_11, R.id.me_radio_group_12,
+            R.id.me_radio_group_13, R.id.me_radio_group_14, R.id.me_radio_group_15, R.id.me_radio_group_16,
+            R.id.me_radio_group_17, R.id.me_radio_group_18, R.id.me_radio_group_19, R.id.me_radio_group_20
     };
 
     final int[] mQuestionTextViews = {
-            R.id.me_question_1,
-            R.id.me_question_2,
-            R.id.me_question_3,
-            R.id.me_question_4,
-            R.id.me_question_5,
-            R.id.me_question_6,
-            R.id.me_question_7,
-            R.id.me_question_8,
-            R.id.me_question_9,
-            R.id.me_question_10,
-            R.id.me_question_11,
-            R.id.me_question_12,
-            R.id.me_question_13,
-            R.id.me_question_14,
-            R.id.me_question_15,
-            R.id.me_question_16,
-            R.id.me_question_17,
-            R.id.me_question_18,
-            R.id.me_question_19,
-            R.id.me_question_20
+            R.id.me_question_1,  R.id.me_question_2,  R.id.me_question_3,  R.id.me_question_4,
+            R.id.me_question_5,  R.id.me_question_6,  R.id.me_question_7,  R.id.me_question_8,
+            R.id.me_question_9,  R.id.me_question_10, R.id.me_question_11, R.id.me_question_12,
+            R.id.me_question_13, R.id.me_question_14, R.id.me_question_15, R.id.me_question_16,
+            R.id.me_question_17, R.id.me_question_18, R.id.me_question_19, R.id.me_question_20
     };
 
     public final int mQuestionAmount = mRadioGroupID.length;
@@ -71,6 +41,8 @@ public class MeQuestionsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_me_questions);
 
+        attemptLoadingResults();
+
         mSubmitButton = (Button) findViewById(R.id.activity_me_next_button);
         mSubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,6 +50,39 @@ public class MeQuestionsActivity extends AppCompatActivity {
                 showResults();
             }
         });
+
+        mPreviousButton = (Button) findViewById(R.id.activity_me_previous_button);
+        mPreviousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startQuizOver();
+            }
+        });
+    }
+
+    private void startQuizOver() {
+        Intent startQuizOverIntent = new Intent(this, WelcomeActivity.class);
+        startActivity(startQuizOverIntent);
+    }
+
+    private void attemptLoadingResults() {
+        SharedPreferences sharedPref = getApplicationContext().getSharedPreferences(
+                ME_PREFS_NAME, Context.MODE_PRIVATE
+        );
+
+        String key;
+        int checked, radioButtonID;
+        for (int i = 0; i > -1; i++) {
+            key = "me" + (i + 1);
+            if (sharedPref.contains(key)) {
+                checked = sharedPref.getInt(key, 0);
+                RadioGroup rg = findViewById(mRadioGroupID[i]);
+                radioButtonID = rg.getChildAt(checked - 1).getId();
+                rg.check(radioButtonID);
+            } else {
+                break;
+            }
+        }
     }
 
     void showResults() {
@@ -105,6 +110,7 @@ public class MeQuestionsActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = sharedPref.edit();
         String key;
 
+        editor.clear();
         for (int i = 0; i < mQuestionAmount; i++) {
             key = "me" + (i + 1);
             editor.putInt(key, mSelectedOptions[i]);
--- app/src/main/java/dev/a3820team/a9to5/WelcomeActivity.java
+++ app/src/main/java/dev/a3820team/a9to5/WelcomeActivity.java
@@ -1,6 +1,8 @@
 package dev.a3820team.a9to5;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
@@ -8,6 +10,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import static dev.a3820team.a9to5.MeQuestionsActivity.ME_PREFS_NAME;
 
 /**
  * Created by sk0g on 10/04/2018.
@@ -22,6 +26,8 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        deletePreviousResults();
 
         mMainText = (TextView) findViewById(R.id.main_screen_long_text);
         mMainText.setText(getText(R.string.welcome_page_middle_text));
@@ -38,5 +44,15 @@ public class WelcomeActivity extends AppCompatActivity {
     void switchToQuiz() {
         Intent quizIntent = new Intent(this, MeQuestionsActivity.class);
         startActivity(quizIntent);
+    }
+
+    private void deletePreviousResults() {
+        SharedPreferences sharedPref = getApplicationContext().getSharedPreferences(
+                ME_PREFS_NAME, Context.MODE_PRIVATE
+        );
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.clear();
+        editor.apply();
     }
 }
