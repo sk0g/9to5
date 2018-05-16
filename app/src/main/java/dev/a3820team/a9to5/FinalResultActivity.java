db39713d16ab132d86bfca9e300dc8e94dd6dd61
Ani Mehta <tenderanger@gmail.com>
2018-05-16 19:39:04 +1000

    Work started on final result layout, added final result activity file

---
+++ app/src/main/java/dev/a3820team/a9to5/FinalResultActivity.java
@@ -0,0 +1,14 @@
+package dev.a3820team.a9to5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FinalResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_result);
+
+    }
+}
