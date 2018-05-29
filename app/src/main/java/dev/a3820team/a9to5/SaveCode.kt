1cf7460445e8b5a8a955245a28ec30ceb391cca5
Ani Mehta <tenderanger@gmail.com>
2018-05-29 15:43:43 +1000

    Added array of characters to SaveCode

--- app/src/main/java/dev/a3820team/a9to5/SaveCode.kt
+++ app/src/main/java/dev/a3820team/a9to5/SaveCode.kt
@@ -8,6 +8,8 @@ class SaveCode {
     val ME_PREFIX  = ME_PREFS_PREFIX
     val ORG_PREFIX = ORG_PREFS_PREFIX
     val QUESTION_AMOUNT = mQuestionAmount
+    val CHARACTERS = arrayOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '-', '+', '=', '!', '@',
+        '#', '$', '%', '^', '&', '*', '(', ')', '<', '>', ';', '/', '?', '|')
 
     fun isValidCode(code: String ): Boolean {
         // Returns whether a code is valid or not
