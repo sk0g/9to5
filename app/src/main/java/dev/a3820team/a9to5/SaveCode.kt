556191aa54b6e4a61f77a706211b6236d37bb4bb
Ani Mehta <tenderanger@gmail.com>
2018-05-29 15:58:05 +1000

    Renamed variable for brevity, finalised and added call for isValid()

--- app/src/main/java/dev/a3820team/a9to5/SaveCode.kt
+++ app/src/main/java/dev/a3820team/a9to5/SaveCode.kt
@@ -4,16 +4,27 @@ import dev.a3820team.a9to5.MeQuestionsActivity.ME_PREFS_PREFIX
 import dev.a3820team.a9to5.MeQuestionsActivity.mQuestionAmount
 import dev.a3820team.a9to5.OrgQuestionsActivity.ORG_PREFS_PREFIX
 
-class SaveCode {
+object SaveCode {
     val ME_PREFIX  = ME_PREFS_PREFIX
     val ORG_PREFIX = ORG_PREFS_PREFIX
     val QUESTION_AMOUNT = mQuestionAmount
-    val CHARACTERS = arrayOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '-', '+', '=', '!', '@',
+    val VALID_CHARACTERS = arrayOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '-', '+', '=', '!', '@',
         '#', '$', '%', '^', '&', '*', '(', ')', '<', '>', ';', '/', '?', '|')
 
+    @JvmStatic
     fun isValidCode(code: String ): Boolean {
-        // Returns whether a code is valid or not
-        return true
+        var isValid = true
+
+        if (code.length != 10)
+            isValid = false
+        else {
+            for (currentCharacter in code) {
+                if (currentCharacter !in VALID_CHARACTERS)
+                    isValid = false
+            }
+        }
+
+        return isValid
     }
 
     fun generateCode(context: Context): String {
